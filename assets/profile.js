06a80943590650cb62f05afcded5fc77e82168a4
Ayush Kumar Agarwal <ayushgrwl365@gmail.com>
2020-04-15 14:20:45 +0530

    fourth commit

---
+++ assets/profile.js
@@ -0,0 +1,6 @@
+$(function () {
+            $(document).scroll(function () {
+                var $nav = $("#mainNavbar");
+                $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+            });
+        });
