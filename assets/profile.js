1b9e6b7053a853c656fd24976d0806e5ad33f711
ayushhagarwal <ayushgrwl365@gmail.com>
2020-07-15 12:50:09 +0530

    few navbar responsive issue fixed

--- assets/profile.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-            $(document).scroll(function () {
-                var $nav = $("#mainNavbar");
-                $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
-            });
-        });
